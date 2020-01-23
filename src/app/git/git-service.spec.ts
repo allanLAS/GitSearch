7d72144c146f5b359925a2a15382281960d2bece
Allan <allanlumumba@gmail.com>
2020-01-23 03:54:52 +0300

    link my github profile to home page

--- src/app/git/git-service.spec.ts
+++ src/app/git/git-service.spec.ts
@@ -1,7 +1,15 @@
 import { GitService } from './git-service';
+import { TestBed, inject } from '@angular/core/testing';
+
 
 describe('GitService', () => {
-  it('should create an instance', () => {
-    expect(new GitService()).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [GitService]
+    });
   });
+
+  it('should be created', inject([GitService], (service: GitService) => {
+    expect(service).toBeTruthy();
+  }));
 });
--- src/app/git/git-service.ts
+++ src/app/git/git-service.ts
@@ -26,4 +26,6 @@ export class GitService {
 
   updateUser(username: string) {
     this.username = username;
+    }
 }
+    
